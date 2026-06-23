23993268c2e3f015bcd4c9771d12e8369b19c534
Lukas Maurer <lukas.maurer@siemens.com>
2026-06-23 16:20:05 +0200

    feat: add sketch card section

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Angular2ImageGalleryModule } from 'angular2-image-gallery';
+import { IxTypography } from '@siemens/ix-angular/standalone';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.scss'],
-  imports: [Angular2ImageGalleryModule],
+  imports: [Angular2ImageGalleryModule, IxTypography],
 })
 export class WorkComponent {}
